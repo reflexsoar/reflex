7ee4f3ec9e96dffa1af1c2fb3d72d6af3506d3c6
Justin Henderson <jhenderson@tekrefresh.com>
2020-09-18 11:01:49 -0500

    a

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,13 +45,11 @@ fi
 
 if $install; then
     os_version=$(hostnamectl | grep "Operating System" | cut -d":" -f2 | cut -d" " -f2-)
-    if [[ "$os_version" == "CentOS"* ]]; then
-    os="centos"
-    yum install -y python3-pip wget unzip
+    if [[ "$os" == "centos" ]]; then
+        yum install -y python3-pip wget unzip
     fi
-    if [[ "$os_version" == "Ubuntu"* ]]; then
-    os="ubuntu"
-    apt install -y python3-pip wget unzip
+    if [[ "$os" == "ubuntu" ]]; then
+        apt install -y python3-pip wget unzip
     fi
     pip3 install pipenv
     wget https://www.hasecuritysolutions.com/reflex_1.0.zip -O /opt/reflex.zip
