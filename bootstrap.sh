a95016aa3ba3dfeda65b1808fa5fc51b9b9edb21
Justin Henderson <jhenderson@tekrefresh.com>
2020-09-18 10:28:05 -0500

    added prereqs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@
 os_version=$(hostnamectl | grep "Operating System" | cut -d":" -f2 | cut -d" " -f2-)
 if [[ "$os_version" == "CentOS"* ]]; then
   os="centos"
+  yum install -y python3-pip
 fi
 if [[ "$os_version" == "Ubuntu"* ]]; then
   os="ubuntu"
+  apt install -y python3-pip
 fi
+pip3 install pipenv
 echo $os
